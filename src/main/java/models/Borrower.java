c2c41cfd5cfb2af0eb849a6e045fdeb4dc0f9d19
Jack Murning <jackrichardmurning@gmail.com>
2018-07-11 14:09:45 +0100

    change from List to Set

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -3,6 +3,7 @@ package models;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "borrowers")
@@ -10,7 +11,7 @@ public class Borrower {
 
     private int id;
     private String name;
-    private List<Book> itemsBorrowed;
+    private Set<Book> itemsBorrowed;
     private Library library;
 
     public Borrower(){
@@ -35,7 +36,7 @@ public class Borrower {
     }
 
     @OneToMany(mappedBy = "currentBorrower")
-    public List<Book> getItemsBorrowed() {
+    public Set<Book> getItemsBorrowed() {
         return itemsBorrowed;
     }
 
@@ -53,7 +54,7 @@ public class Borrower {
         this.name = name;
     }
 
-    public void setItemsBorrowed(List<Book> itemsBorrowed) {
+    public void setItemsBorrowed(Set<Book> itemsBorrowed) {
         this.itemsBorrowed = itemsBorrowed;
     }
 
