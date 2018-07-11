0c881976425b6f5370762cb50853b244827b5ed8
cjmcd123 <cj_mcdowall@btinternet.com>
2018-07-11 15:32:46 +0100

    add controller to maincontroller & created views

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -4,11 +4,18 @@ import db.*;
 import models.*;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import static spark.SparkBase.staticFileLocation;
+
 public class MainController {
 
     public static void main(String[] args) {
+
+//        staticFileLocation("/public");
+
+
         VelocityTemplateEngine velocityTemplateEngine =new VelocityTemplateEngine();
         BookController bookController =new BookController();
+        BorrowerController borrowerController = new BorrowerController();
         Seeds.seedData();
 
 
