9e269f2f71379eb2e9f6b25579c62765c8c3ed04
cjmcd123 <cj_mcdowall@btinternet.com>
2018-07-11 12:03:58 +0100

    started borrower class

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,4 +1,42 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Borrower {
+
+    private int id;
+    private String name;
+    private ArrayList<Book> itemsBorrowed;
+
+    public Borrower(){
+
+    }
+
+    public Borrower(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Book> getItemsBorrowed() {
+        return itemsBorrowed;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setItemsBorrowed(ArrayList<Book> itemsBorrowed) {
+        this.itemsBorrowed = itemsBorrowed;
+    }
 }
