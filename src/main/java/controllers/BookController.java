7fd1becdc49ca35e9bb1bd4fb5c979e742010403
Jack Murning <jackrichardmurning@gmail.com>
2018-07-11 14:14:06 +0100

    add bookcontroller file

---
+++ src/main/java/controllers/BookController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import spark.template.velocity.VelocityTemplateEngine;
+
+public class BookController {
+    VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+
+    public BookController(){this.setupEndpoint();}
+
+    private void setupEndpoint(){
+
+    }
+}
