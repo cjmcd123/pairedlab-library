441c2d87dc604b96b760db1d9c48c2cdc0a05883
cjmcd123 <cj_mcdowall@btinternet.com>
2018-07-11 15:08:07 +0100

    created controllers

--- src/main/java/db/DBLibrary.java
+++ src/main/java/db/DBLibrary.java
@@ -6,7 +6,9 @@ import models.Borrower;
 
 public class DBLibrary {
 
-    public void borrow(Borrower borrower, Book book){
+
+
+    public static void borrow(Borrower borrower, Book book){
         if (book.lendAble()) {
             borrower.borrow(book);
             book.lend(borrower);
@@ -14,7 +16,7 @@ public class DBLibrary {
         }
     }
 
-    public void returnBook(Borrower borrower, Book book){
+    public static void returnBook(Borrower borrower, Book book){
         borrower.returnBook(book);
         book.returnBook();
         DBHelper.save(book);
