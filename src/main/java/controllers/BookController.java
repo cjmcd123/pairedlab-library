c96abbc80f2160f753ff802a478b8c09b09d916d
Jack Murning <jackrichardmurning@gmail.com>
2018-07-11 14:26:55 +0100

    add get methoth for bookcontroller /books

--- src/main/java/controllers/BookController.java
+++ src/main/java/controllers/BookController.java
@@ -1,6 +1,14 @@
 package controllers;
 
+import db.DBHelper;
+import models.Book;
+import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static spark.Spark.get;
 
 public class BookController {
     VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
@@ -8,6 +16,13 @@ public class BookController {
     public BookController(){this.setupEndpoint();}
 
     private void setupEndpoint(){
+        get("/books", (req, res)->{
+            HashMap<String , Object> model = new HashMap<>();
+            List<Book> books = DBHelper.getAll(Book.class);
+            model.put("template", "templates/book/index.vtl");
+            model.put("books", books);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
 
     }
 }
